c449732c00426fe3ef399ccdc6dd64fcedd84ee5
natanaelbatista99 <natanaelfdbatista99@gmail.com>
2019-09-05 09:52:42 -0300

    Primeiro commit

---
+++ dnseq.py
@@ -0,0 +1,36 @@
+import time
+inicio = time.time()
+
+import sys
+import dns.resolver
+
+argumentos = sys.argv #le os argumentos do comando
+
+try:
+	dominio = argumentos[1]
+	lista = argumentos[2]
+except:
+	print("Faltam argumentos no comando")
+	sys.exit(1)
+
+#abre a wordlist
+try:
+	arquivo = open(lista)
+	linhas = arquivo.read().splitlines()
+except:
+	print("Arquivo nao encontrado ou ivalido")
+	sys.exit(1)
+
+#para cada linha da wordlist testa o dns
+for linha in linhas:
+    subdominio = linha + '.' + dominio
+    try:
+        respostas = dns.resolver.query(subdominio, 'a')
+        for resultado in respostas:
+            print (subdominio, resultado)
+    except:
+        pass
+
+fim = time.time()
+print ('Time: ')
+print (round((fim-inicio), 4))
---
+++ dnsthreads.py
@@ -0,0 +1,66 @@
+import time
+inicio = time.time()
+
+import sys
+import dns.resolver
+import logging
+import time
+from threading import Thread
+import math
+
+global i
+global tmpfinal
+
+i = tmpfinal = 0
+
+def test(t, lista):
+	global i
+	global tmpfinal
+	logging.info(lista)
+	dominio = 'xytimtumtom.com'
+ 
+	try:
+		arquivo = open(lista)
+		linhas = arquivo.read().splitlines()
+	except:
+		print("Arquivo nao encontrado ou ivalido")
+		sys.exit(1)
+
+	for linha in linhas:
+		subdominio = linha + '.' + dominio
+		try:
+			respostas = dns.resolver.query(subdominio, 'a')
+			for resultado in respostas:
+				print (subdominio, resultado)
+		except:
+			pass
+	i = i + 1
+	fim = time.time()
+	print ('Time %d: ' % t)
+	tmpfinal = max(tmpfinal, (fim-inicio))
+	print (round((fim-inicio), 4))
+
+	if(i == 5):
+		fim = time.time()
+		print ('\nTime Final')
+		print (round(tmpfinal, 4))
+    	
+
+
+format = "%(asctime)s: %(message)s"
+logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
+
+t1 = Thread(target=test, args=(1, 'lt1.txt',))
+t1.start()
+
+t2 = Thread(target=test, args=(2, 'lt2.txt',))
+t2.start()
+
+t3 = Thread(target=test, args=(3, 'lt3.txt',))
+t3.start()
+
+t4 = Thread(target=test, args=(4, 'lt4.txt',))
+t4.start()
+
+t5 = Thread(target=test, args=(5, 'lt5.txt',))
+t5.start()
